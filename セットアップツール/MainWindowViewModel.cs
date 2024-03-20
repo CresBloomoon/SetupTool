b83c42bbf93168e69f1c75ddb043aa3bec95b12b
CresBloomoon <metroidm2421@gmail.com>
2024-03-20 21:32:32 +0900

    プロジェクト ファイルを追加します。

---
+++ セットアップツール/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using セットアップツール.ComboBox;
+
+namespace セットアップツール {
+    internal sealed class MainWindowViewModel {
+
+        public Dictionary<WeatherType, string> WeatherNames { get; set; }
+
+        public MainWindowViewModel() {
+            WeatherNames = new Dictionary<WeatherType, string> {
+                [WeatherType.Sunny] = "晴れ",
+                [WeatherType.Cloudy] = "曇り",
+                [WeatherType.Rainy] = "雨",
+                [WeatherType.Snow] = "雪",
+            };
+        }
+
+        public string SelectItem { get; set; }
+    }
+}
---
+++ セットアップツール/Validations/NumberValidationRule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace セットアップツール.Validations {
+    public class NumberValidationRule : ValidationRule {
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+        public bool NotEmpty { get; set; }
+        public string MessageHeader { get; set; }
+
+        public NumberValidationRule() {
+            MinValue = double.MinValue;
+            MaxValue = double.MaxValue;
+            NotEmpty = false;
+            MessageHeader = null;
+        }
+
+        public NumberValidationRule(double min, double max, bool notEmpty = false, string msgHeader = null) {
+            MinValue = min;
+            MaxValue = max;
+            NotEmpty = notEmpty;
+            MessageHeader = msgHeader;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            if (null == value) {
+                if (NotEmpty) {
+                    var msg = "値を入力してください。";
+                    if (!string.IsNullOrWhiteSpace(MessageHeader))
+                        msg = MessageHeader + "に" + msg;
+                    return new ValidationResult(false, msg);
+                }
+                else
+                    return ValidationResult.ValidResult;
+            }
+            string str = value.ToString();
+            if (string.IsNullOrEmpty(str)) {
+                if (NotEmpty) {
+                    var msg = "値を入力してください。";
+                    if (!string.IsNullOrWhiteSpace(MessageHeader))
+                        msg = MessageHeader + "に" + msg;
+                    return new ValidationResult(false, msg);
+                }
+                else
+                    return ValidationResult.ValidResult;
+            }
+
+            double ret;
+            if (!double.TryParse(str, out ret)) {
+                var msg = "数値を入力してください。";
+                if (!string.IsNullOrWhiteSpace(MessageHeader))
+                    msg = MessageHeader + "に" + msg;
+                return new ValidationResult(false, msg);
+            }
+            if ((MinValue > ret) || (MaxValue < ret)) {
+                var msg = "値が範囲外です。(" + MinValue + "～" + MaxValue + ")";
+                if (!string.IsNullOrWhiteSpace(MessageHeader))
+                    msg = MessageHeader + "の" + msg;
+                return new ValidationResult(false, msg);
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
